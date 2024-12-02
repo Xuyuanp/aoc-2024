95fd7ac424e77723347234f1113327ba71e3c74f
XuYuan Pang <pangxuyuan@itiger.com>
2024-12-02 18:26:55 +0800

    day1 part2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,4 +1,4 @@
-use std::iter::zip;
+use std::{collections::HashMap, iter::zip};
 
 advent_of_code::solution!(1);
 
@@ -28,8 +28,26 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(res)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut counter = HashMap::<u32, u32>::new();
+
+    let col1 = input
+        .lines()
+        .map(|line| {
+            let numbers: Vec<u32> = line
+                .split_whitespace()
+                .map(|word| word.parse::<u32>().unwrap())
+                .collect();
+            assert_eq!(numbers.len(), 2);
+            let (a, b) = (numbers[0], numbers[1]);
+            let cnt = counter.entry(b).or_insert(0);
+            *cnt += 1;
+            a
+        })
+        .collect::<Vec<u32>>();
+
+    let res = col1.iter().map(|a| counter.get(a).unwrap_or(&0) * a).sum();
+    Some(res)
 }
 
 #[cfg(test)]
@@ -45,6 +63,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(31));
     }
 }
