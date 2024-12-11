57b2b3557410a8a4ad121a8553870657d6ba6975
XuYuan Pang <pangxuyuan@itiger.com>
2024-12-11 11:53:09 +0800

    day 5, part 2

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,24 +1,26 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap, HashSet, VecDeque};
 
 advent_of_code::solution!(5);
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let mut deps: HashMap<u32, HashSet<u32>> = HashMap::new();
-
+fn parse_dependencies(input: &str) -> HashMap<u32, HashSet<u32>> {
     input
         .lines()
         .take_while(|line| !line.is_empty())
         .map(|line| {
             let nums = line
-                .split("|")
+                .split('|')
                 .map(|s| s.parse::<u32>().unwrap())
                 .collect::<Vec<u32>>();
             (nums[0], nums[1])
         })
-        .for_each(|(x, y)| {
-            let ent = deps.entry(y).or_default();
-            ent.insert(x);
-        });
+        .fold(HashMap::new(), |mut deps, (x, y)| {
+            deps.entry(y).or_default().insert(x);
+            deps
+        })
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let deps = parse_dependencies(input);
 
     let res = input
         .lines()
@@ -31,11 +33,10 @@ pub fn part_one(input: &str) -> Option<u32> {
         })
         .filter(|nums| {
             let mut visited = HashSet::new();
-            let curr_update: HashSet<u32> = HashSet::from_iter(nums.iter().cloned());
             for num in nums {
                 if let Some(upstreams) = deps.get(num) {
                     for upstream in upstreams {
-                        if curr_update.contains(upstream) && !visited.contains(upstream) {
+                        if nums.contains(upstream) && !visited.contains(upstream) {
                             return false;
                         }
                     }
@@ -50,7 +51,45 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let deps = parse_dependencies(input);
+
+    let res = input
+        .lines()
+        .skip_while(|line| !line.is_empty()) // skip rules
+        .skip(1) // skip empty line
+        .map(|line| {
+            line.split(",")
+                .map(|s| s.parse::<u32>().unwrap())
+                .collect::<Vec<u32>>()
+        })
+        .filter_map(|nums| {
+            let mut new_nums = vec![];
+            let mut changed = false;
+            let mut queue = VecDeque::from(nums.clone());
+
+            while let Some(num) = queue.pop_front() {
+                if let Some(upstreams) = deps.get(&num) {
+                    let ok = upstreams
+                        .iter()
+                        .filter(|upstream| nums.contains(upstream)) // upstream in this update
+                        .all(|upstream| new_nums.contains(upstream)); // upstream already visited
+                    if !ok {
+                        changed = true;
+                        queue.push_back(num);
+                        continue;
+                    }
+                }
+                new_nums.push(num);
+            }
+
+            if changed {
+                Some(new_nums[new_nums.len() / 2])
+            } else {
+                None
+            }
+        })
+        .sum();
+    Some(res)
 }
 
 #[cfg(test)]
@@ -66,6 +105,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(123));
     }
 }
